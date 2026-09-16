b90cdadd2311c37e9ab626e4f2c52a06f4eaab88
ACER <ACER@LAPTOP-F54L9TLP>
2026-09-16 17:02:20 +0700

    Cap nhat giao dien login hieu ung

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './style.css' // hoặc đường dẫn chính xác đến file css của bạn
 // Import CSS của thư viện Pannellum dùng cho không gian 360 độ
 import 'pannellum/build/pannellum.css';
 
